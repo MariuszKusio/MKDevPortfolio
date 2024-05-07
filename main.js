e8c19ca692a894b7c2aebc89cff9940c1f1b7073
MariuszKusio <91279272+MariuszKusio@users.noreply.github.com>
2024-05-07 19:00:08 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -392,3 +392,47 @@ Scrollbar.init(document.querySelector('.projectView'), options);
 // Napraw scrollowanie w prawo szczegółów projektu po ich przełęczaniu ponieważ to nie działa.
 
 
+// Text reveal
+
+// pobierz dwa descriptiony poprzez querySelectorAll i wykorzystaj foreach to nadania im spanów
+let descriptionParagraph = document.querySelector('.heroParagraphText');
+let spans = [];
+
+const spanTextWrapper = () => {
+   let htmlString = '';
+   let pArray = descriptionParagraph.textContent.split('');
+
+   for(let i=0; i < pArray.length; i++) {
+    htmlString += `<span>${pArray[i]}</span>`;
+
+    // descriptionParagraph.innerHTML = htmlString;
+   }
+    
+   descriptionParagraph.innerHTML = htmlString;
+   console.log(pArray);
+};
+window.addEventListener('DOMContentLoaded', spanTextWrapper());
+
+
+spans = [...document.querySelectorAll('span')];
+
+const textReveal = () => {
+  for(let i=0; i < spans.length; i++){
+    if(spans[i].parentElement.getBoundingClientRect().top < window.innerHeight /2) {
+      let {left, top} = spans[i].getBoundingClientRect();
+      top = top - (window.innerHeight * .4);
+      let opacityValue = 1 - ((top * .01) + (left * 0.001)) < 0.1 ? 0.1 : 1 - ((top * .01) + (left * 0.001)).toFixed(3);
+      opacityValue = opacityValue > 1 ? 1 : opacityValue.toFixed(3);
+      spans[i].style.opacity = opacityValue;
+
+
+    }
+  }
+}
+
+
+window.addEventListener('scroll', () => {
+  textReveal()
+});
+
+textReveal();
